79c1ebee6cc90501eceda8865a97cf1dc5926263
David Montoya <deividelpaisa@hotmail.com>
2024-03-16 14:35:19 -0500

    progress indicate terminado

--- lib/menu/menu_items.dart
+++ lib/menu/menu_items.dart
@@ -34,4 +34,10 @@ const appMenuItems = <MenuItem>[
     link: '/cards',
     icon: Icons.credit_card,
   ),
+  MenuItem(
+    title: 'ProgessIndicators',
+    subtitle: 'Generales y controlados',
+    link: '/progress',
+    icon: Icons.refresh_rounded,
+  ),
 ];
