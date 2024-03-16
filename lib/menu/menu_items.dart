57052e6f4a69ca66bc2b4761bedd3f2800590594
David Montoya <deividelpaisa@hotmail.com>
2024-03-16 15:57:55 -0500

    ui screen terminado

--- lib/menu/menu_items.dart
+++ lib/menu/menu_items.dart
@@ -52,4 +52,10 @@ const appMenuItems = <MenuItem>[
     link: '/animated',
     icon: Icons.check_box_outline_blank,
   ),
+  MenuItem(
+    title: 'UI Controls + Tiles',
+    subtitle: 'Una serie de controles de Flutter',
+    link: '/ui-controls',
+    icon: Icons.car_rental_outlined,
+  ),
 ];
