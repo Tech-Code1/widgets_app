6572e176f8240211252aa7f45101a7f70fdd7626
David Montoya <deividelpaisa@hotmail.com>
2024-03-16 16:02:04 -0500

    imagenes agregadas

--- lib/menu/menu_items.dart
+++ lib/menu/menu_items.dart
@@ -58,4 +58,10 @@ const appMenuItems = <MenuItem>[
     link: '/ui-controls',
     icon: Icons.car_rental_outlined,
   ),
+  MenuItem(
+    title: 'Introducción a la aplicación',
+    subtitle: 'Pequeño tutorial introductorio',
+    link: '/tutorial',
+    icon: Icons.accessibility_rounded,
+  ),
 ];
